7c30f638497675ef26905d80d6fc42621bf6f4dd
elaabramovi <abramova17ella@gmail.com>
2024-12-04 14:41:30 +0400

    exam

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const translations = {
         title: "კრეატიული გრაფიკული დიზაინი",
         subtitle: "პროფესიონალური დიზაინი თქვენი ბრენდისთვის.",
         "services-title": "სერვისები",
-        "contact-title": "დაგვიკავშირდით",
+        "service-title": "ბრენდინგი",
+        "contact-title": "კონტაქტი",
         "footer-text": "© 2024 ElGraphiq. All rights reserved."
     },
     en: {
         title: "Creative Graphic Design",
         subtitle: "Professional design services that elevate your brand.",
         "services-title": "Services",
-        "contact-title": "Contact Us",
+        "service-title": "Branding",
+        "contact-title": "Contact Info",
         "footer-text": "© 2024 ElGraphiq. All rights reserved."
     },
 };
@@ -19,6 +21,7 @@ function setLanguage(language) {
     document.getElementById("title").textContent = translations[language].title;
     document.getElementById("subtitle").textContent = translations[language].subtitle;
     document.getElementById("services-title").textContent = translations[language]["services-title"];
+    document.getElementById("service-title").textContent = translations[language]["service-title"];
     document.getElementById("contact-title").textContent = translations[language]["contact-title"];
     document.getElementById("footer-text").textContent = translations[language]["footer-text"];
 }
